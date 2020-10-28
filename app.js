a163a08c230ef8332d990ef1c75a75eef157a331
KoyenaPal <koyena_pal@brown.edu>
2020-10-28 18:43:45 -0400

    Completed lab

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=REDACTED_TOKEN";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,45 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      console.log("After promise");
+      //console.log(request.response);
+      handleResponse(request.response)
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+const displayDiv = () => {
+  const wrapper = document.createElement("div");
+  wrapper.setAttribute("id","wrapper");
+  const header = document.createElement("h1");
+  header.innerText = "Hi";
+  wrapper.appendChild(header);
+  document.getElementById("plants").appendChild(wrapper);
+};
+
+const displayResponse = (plant) => {
+  const wrapper = document.createElement("div");
+  const something = document.createElement("p");
+  something.innerText = plant;
+  wrapper.appendChild(something);
+  document.getElementById("plantsinfo").appendChild(wrapper);
+};
+
+
+
+const handleResponse = (requestResponse) => {
+  const jsonified = JSON.parse(requestResponse);
+  const plantsArray = jsonified.data;
+  console.log(plantsArray)
+
+  const plantsAfter1753 = plantsArray.filter((arrayItem) => {
+    return (arrayItem.year > 1753);
+  })
+  console.log(plantsAfter1753)
+
+  plantsAfter1753.map((arrayItem) => {
+    displayResponse(arrayItem.common_name)
+    console.log(arrayItem.year);
+  })
+}
